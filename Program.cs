a1afe430a118d6ab6f5870386e4145c402cec9e5
Fajar22isworo <61859353+Fajar22isworo@users.noreply.github.com>
2020-06-13 22:12:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//using LatihanOOP_Abstrak.AbstrackClass;
+using LatihanOOP_Abstrak.Interface; 
+
+namespace LatihanOOP_Abstrak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Makanan Makanan;
+
+            Makanan = new Kentang();
+            Makanan.Dimasak();
+
+            Console.WriteLine();
+            Makanan = new Kangkung();
+            Makanan.Dimasak();*/
+
+            IMakanan Makanan;
+
+            Makanan = new Kentang();
+            Makanan.Dimasak();
+
+            Console.WriteLine();
+            Makanan = new Kangkung();
+            Makanan.Dimasak();
+
+            Console.ReadKey ();
+        }
+    }
+}
